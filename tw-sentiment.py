8caa97adafab72f0c4af03a20eec57211f9b594d
mary <mevere66@gmail.com>
2022-11-07 13:12:22 -0500

    analisis de sentimiento en tweet

---
+++ tw-sentiment.py
@@ -0,0 +1,67 @@
+from transformers import AutoTokenizer, AutoModelForSequenceClassification
+from scipy.special import softmax
+import tweepy
+import configparser
+
+# read configs
+config = configparser.ConfigParser()
+config.read('config.ini')
+
+api_key = config['twitter']['api_key']
+api_key_secret = config['twitter']['api_key_secret']
+
+access_token = config['twitter']['access_token']
+access_token_secret = config['twitter']['access_token_secret']
+
+# authentication
+auth = tweepy.OAuthHandler(api_key, api_key_secret)
+auth.set_access_token(access_token, access_token_secret)
+
+api = tweepy.API(auth)
+
+# public_tweets = api.home_timeline()
+
+# tweet= public_tweets[0].text
+# print('Latest tweet: '+ tweet)
+# print(tweet)
+
+# tweet = "@MehranShakarami today's cold @ home 😒 https://mehranshakarami.com"
+# tweet = 'amo los dias soleados! 😉'
+# tweet = 'I do not know what to do'
+tweet = 'Nadie: Yo: y si me asoleo el ano? 🤔'
+print(tweet)
+
+# precprcess tweet
+tweet_words = []
+
+for word in tweet.split(' '):
+    if word.startswith('@') and len(word) > 1:
+        word = '@user'
+    
+    elif word.startswith('http'):
+        word = "http"
+    tweet_words.append(word)
+
+tweet_proc = " ".join(tweet_words)
+
+# load model and tokenizer
+roberta = "cardiffnlp/twitter-roberta-base-sentiment"
+
+model = AutoModelForSequenceClassification.from_pretrained(roberta)
+tokenizer = AutoTokenizer.from_pretrained(roberta)
+
+labels = ['Negative', 'Neutral', 'Positive']
+
+# sentiment analysis
+encoded_tweet = tokenizer(tweet_proc, return_tensors='pt')
+# output = model(encoded_tweet['input_ids'], encoded_tweet['attention_mask'])
+output = model(**encoded_tweet)
+
+scores = output[0][0].detach().numpy()
+scores = softmax(scores)
+
+for i in range(len(scores)):
+    
+    l = labels[i]
+    s = scores[i]
+    print('Tweet was ',s ,' %',l )
